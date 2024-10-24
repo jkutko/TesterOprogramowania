1a620e5f1595f139296b032e6f8f23bd805aaf07
utkoj <justyna.utko@ttpsc.com>
2024-10-24 12:22:38 +0200

    pierwszy

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+
+    public void otworz(){
+        System.out.println("Hello");
+    }
+}
