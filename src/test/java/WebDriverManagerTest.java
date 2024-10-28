77771ed23a5f191439a67f84796db0ebf85674c6
utkoj <justyna.utko@ttpsc.com>
2024-10-28 13:13:14 +0100

    opcje przegladarki

--- src/test/java/WebDriverManagerTest.java
+++ src/test/java/WebDriverManagerTest.java
@@ -1,6 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -8,10 +11,19 @@ public class WebDriverManagerTest {
 
     @Test
     public void OpenBrowser(){
-        WebDriverManager.firefoxdriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("alert('Hello')");
+        driver.get("https://google.com");
         //driver.close();
+
+
 
 
 
